348d07a940be816b2abd149154b74b4ade21a5f2
Brunei Paul Potosi Ccuno <neoxtrotoka@gmail.com>
2024-04-12 00:45:45 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+import AppContentHere from './components/AppContentHere.vue';
+import AppServices from './components/AppServices.vue'
+export default {
+  components: {
+    AppHeader,
+    AppContentHere,
+    AppServices
+  }
+}
+</script>
+
+<template>
+  <AppHeader></AppHeader>
+  <ContentHere></ContentHere>
+  <main>
+  </main>
+  <AppServices></AppServices>
+</template>
+
+<style lang="scss">
+@use './style/generic';
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,70 @@
+<script>
+export default {
+    name: 'AppHeader'
+}
+</script>
+
+<template>
+<header>
+    <div class="logo">
+      <img src="../assets/img/dc-logo.png" alt="Logo">
+    </div>
+    <nav>
+      <ul>
+        <li><a href="#">CHARACTERS</a></li>
+        <li><a href="#">COMICS</a></li>
+        <li><a href="#">MOVIES</a></li>
+        <li><a href="#">TV</a></li>
+        <li><a href="#">GAMES</a></li>
+        <li><a href="#">COLLECTIVES</a></li>
+        <li><a href="#">VIDEOS</a></li>
+        <li><a href="#">FANS</a></li>
+        <li><a href="#">NEWS</a></li>
+        <li><a href="#">SHOP</a></li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<style scoped lang="scss">
+
+@use '../style/partials/variables' as *;
+
+
+header {
+  font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  padding: 20px;
+  background-color: #ece1e1;
+  color: #fff;
+  font-size: 17px;
+}
+
+.logo img {
+  width: 80px;
+}
+
+nav ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  display: flex;
+}
+
+nav ul li {
+  margin-left: 20px; 
+}
+
+
+nav ul li a {
+  text-decoration: none;
+  color: $brand_secondary;
+}
+
+a:hover{
+    color: $brand_primary;
+}
+
+</style>
